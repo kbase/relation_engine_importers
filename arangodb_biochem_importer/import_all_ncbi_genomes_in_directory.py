288ee6e05c28e778cffbb62e135493ef479ade7d
Jay R Bolton <jayrbolton@gmail.com>
2018-10-17 13:41:43 -0700

    Clean up some logging and failure logs in import_all_ncbi_genomes

--- arangodb_biochem_importer/import_all_ncbi_genomes_in_directory.py
+++ arangodb_biochem_importer/import_all_ncbi_genomes_in_directory.py
@@ -3,39 +3,40 @@ import sys
 import os
 import time
 
-from import_ncbi_genome import import_genomes, setup
+import import_ncbi_genome as importer
 
 
-def main():
-    start = int(time.time() * 1000)
-    if not len(sys.argv) >= 2:
-        sys.stderr.write('Pass in the directory path of many subdirectories containing genbank files.\n')
-        sys.exit(1)
-    parent_dir = sys.argv[1]
-    if not os.path.isdir(parent_dir):
-        sys.stderr.write('The parent directory %s does not exist\n' % parent_dir)
-        sys.exit(1)
-    setup()
-    failures = []  # type: list
+def main(parent_dir):
+    failures_log_path = os.path.join(parent_dir, 'failures.log')
+    failures_log = open(failures_log_path, 'w')
+    import_count = 0
     for subdir in os.listdir(parent_dir):
         if not re.search(r'GCF_\d\d\d\d\d\d\d\d\d\.\d', subdir):
+            failures_log.write('Invalid subdirectory name %s\n' % subdir)
             # Subdirectory must match GCF accession ID format
             continue
         full_path = os.path.join(parent_dir, subdir)
         print('importing genome in %s' % subdir)
         try:
-            import_genomes(full_path)
+            importer.import_genomes(full_path)
         except Exception as err:
-            failures.append(str(err))
-        print('  done importing %s' % subdir)
-    # Write failed imports out to a log file
-    failures_file_path = os.path.join(parent_dir, 'failures.log')
-    with open(failures_file_path, 'w') as fd_write:
-        fd_write.write('\n'.join(failures))
-    print('wrote failed imports to %s' % failures_file_path)
-    end = int(time.time() * 1000)
-    print('total time running in ms: %s' % (end - start))
+            failures_log.write('Failed to import %s\t%s' % (subdir, str(err)))
+    print('successfully imported %s genomes' % import_count)
+    print('wrote failed imports to %s' % failures_log_path)
+    failures_log.close()
 
 
 if __name__ == '__main__':
-    main()
+    if not len(sys.argv) >= 2:
+        sys.stderr.write('Pass in the directory path with many subdirectories containing genbank files.\n')
+        sys.exit(1)
+    parent_dir = sys.argv[1]
+    if not os.path.isdir(parent_dir):
+        sys.stderr.write('The parent directory %s does not exist\n' % parent_dir)
+        sys.exit(1)
+    print('running..')
+    importer.setup()
+    start = int(time.time() * 1000)
+    main(parent_dir)
+    end = int(time.time() * 1000)
+    print('total time running in ms: %s' % (end - start))
