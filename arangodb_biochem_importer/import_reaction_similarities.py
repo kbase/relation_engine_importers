fcbfc760af4286c6187107a4e52e383a0bccaa84
Jay R Bolton <jayrbolton@gmail.com>
2018-12-07 13:49:31 -0800

    Tweak the reaction similarity importer for the new data

--- arangodb_biochem_importer/import_reaction_similarities.py
+++ arangodb_biochem_importer/import_reaction_similarities.py
@@ -5,7 +5,8 @@ import csv
 
 from init_db import init_db
 
-similarity_threshold = 0.0
+_similar_edge_name = 'rxn_similar_to_reaction'
+_reaction_vert_name = 'rxn_reaction'
 
 
 def import_similarities(file_path):
@@ -13,15 +14,17 @@ def import_similarities(file_path):
     Iterate over every row in a TSV file and import each as a compound document.
     We don't yet create edges for compounds.
     """
-    reaction_similar_to = db.collections['reaction_similar_to']
+    reaction_similar_to = db.collections[_similar_edge_name]
     with open(file_path, newline='') as csv_fd:
         reader = csv.reader(csv_fd, delimiter=' ', quotechar='"')
         to_insert = []
         for row in reader:
-            row = ['reactions/' + row[0], 'reactions/' + row[1], float(row[2])]
-            if row[2] < similarity_threshold:
-                continue
-            doc = {'_from': row[0], '_to': row[1], 'similarity': row[2]}
+            doc = {
+                '_from': _reaction_vert_name + '/' + row[0],
+                '_to': _reaction_vert_name + '/' + row[1],
+                'sf_similarity': float(row[2]),
+                'df_similarity': float(row[3])
+            }
             to_insert.append(doc)
     print('importing %s total reaction similarities' % len(to_insert))
     result = reaction_similar_to.bulkSave(to_insert, onDuplicate="replace")
@@ -32,15 +35,16 @@ def import_similarities(file_path):
 if __name__ == '__main__':
     """
     Simple command-line API:
-    python arangodb_biochem_importer/import_compounds.py <path-to-directory-of-compound-files>
-
-    Where all compound files are TSVs with the file name '*compounds.tsv'
+    python arangodb_biochem_importer/import_reaction_similarities.py <path-to-file>
     """
     start = int(time.time() * 1000)
-    if len(sys.argv) <= 1:
-        sys.stderr.write('Provide the file path of a TSV containing similarity data.')
+    if len(sys.argv) < 2:
+        sys.stderr.write('Provide the file path of a csv containing similarity data.\n')
         exit(1)
     db = init_db()
     file_path = os.path.abspath(sys.argv[1])
+    if not os.path.exists(file_path):
+        sys.stderr.write('%s does not exist\n' % file_path)
+        exit(1)
     result = import_similarities(file_path)
     print('total running time in ms: %d' % (int(time.time() * 1000) - start))
